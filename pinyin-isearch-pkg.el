a2222523fb572fc9a904142fc2e27a2893d92960
none <none>
2023-12-25 03:12:42 +0000

    v0.8 after all recheckings 2

--- pinyin-isearch-pkg.el
+++ pinyin-isearch-pkg.el
@@ -4,4 +4,4 @@
     "Allow to search Chinese pinyin without tones"
     '((emacs "27.2"))
     :url "https://github.com/Anoncheg1/pinyin-isearch"
-    :keywords '("chinese" "pinyin" "pinyin-isearch"))
+    :keywords '("chinese" "pinyin" "search"))
