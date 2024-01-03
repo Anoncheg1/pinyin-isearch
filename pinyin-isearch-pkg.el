5fc47fbe2a74680d6614008094034686f6ce1d7a
none <none>
2024-01-03 12:01:01 +0000

    remove pinyin-isearch-pkg.el

--- pinyin-isearch-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package
-    "pinyin-isearch"
-    "1.2"
-    "Allow to search Chinese pinyin without tones"
-    '((emacs "27.2"))
-    :url "https://github.com/Anoncheg1/pinyin-isearch"
-    :keywords '("chinese" "pinyin" "matching" "convenience"))
