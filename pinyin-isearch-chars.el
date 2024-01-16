bbaf1c5f35fd73a2fc54789927a5405432425c99
none <none>
2024-01-16 21:42:51 +0000

    v1.6.2

--- pinyin-isearch-chars.el
+++ pinyin-isearch-chars.el
@@ -5,7 +5,7 @@
 ;; Author: Anoncheg1
 ;; Keywords: chinese, pinyin, matching, convenience
 ;; URL: https://github.com/Anoncheg1/pinyin-isearch
-;; Version: 1.6.1
+;; Version: 1.6.2
 ;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is not part of GNU Emacs.
